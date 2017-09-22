1180bbadb1e513095bdf1e4d9b39d09c57f8ae20
LiaoCJ <337857611@qq.com>
2017-09-22 16:52:54 +0800

    Day 6

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'lcj60135601',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
